6f679130c7c6dd3c9771e296161f1c5f9971fcd6
Kshitij_Mayank <kshitij.feb9@gmail.com>
2021-10-02 17:02:34 -0400

    Time: 24 ms (93.09%), Space: 14.3 MB (15.34%) - LeetHub

--- break-a-palindrome/break-a-palindrome.py
+++ break-a-palindrome/break-a-palindrome.py
@@ -1,10 +1,13 @@
 class Solution:
     def breakPalindrome(self, palindrome: str) -> str:
-        n = len(palindrome)
-        if n == 1: return ''
-        for i, char in enumerate(palindrome):
-            if char != 'a' and i != n//2:
-                return palindrome[:i] + 'a' + palindrome[i+1:]
-            elif char == 'a' and i == n - 1:
-                return palindrome[:-1] + 'b'
-        
+        if len(palindrome) <= 1: return ''
+        isAllA = True
+        target = len(palindrome) # target index to be converted into 'a'
+        for i in range(len(palindrome) // 2):
+            if palindrome[i] != 'a':
+                isAllA = False
+                target = min(target, i) # target should be the letter shown earlier
+		# if all letters are 'a', just switch the last letter to 'b'
+        if isAllA: return palindrome[:len(palindrome) - 1] + 'b'
+		# if not, replace the target letter with 'a' and attach the rest
+        return palindrome[:target] + 'a' + palindrome[target + 1:]
