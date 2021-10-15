dbc09fe394b56d917f289059c4ed428dc17301ce
Kshitij_Mayank <kshitij.feb9@gmail.com>
2021-10-14 23:37:09 -0400

    Time: 143 ms (47.37%), Space: 14.4 MB (70.45%) - LeetHub

--- first-unique-character-in-a-string/first-unique-character-in-a-string.py
+++ first-unique-character-in-a-string/first-unique-character-in-a-string.py
@@ -1,20 +1,24 @@
-from collections import defaultdict
+
 class Solution:
     def firstUniqChar(self, s: str) -> int:
         
-        d = defaultdict(int)
+        _dict = {}
         seen = set()
-        for i,val in enumerate(s):
+        
+        for idx,val in enumerate(s):
+            
             if val not in seen:
                 seen.add(val)
-                d[val] = i
-            elif val in seen and val in d:
-                del d[val]
-        if d :
-            
-            return min(d.values()) 
+                _dict[val] = idx
+            elif val in seen and val in _dict:
+                del _dict[val]
+        
+        if _dict:
+            return min(_dict.values())
         else:
             return -1
+                
+        
                 
             
             
