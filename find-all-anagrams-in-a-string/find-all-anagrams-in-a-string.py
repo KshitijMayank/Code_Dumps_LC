77adc3b224dbb807ffd29f6fb4d65d66cd5b6b8b
Kshitij_Mayank <kshitij.feb9@gmail.com>
2021-10-19 18:31:59 -0400

    Time: 172 ms (45.51%), Space: 15.2 MB (76.90%) - LeetHub

--- find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
+++ find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
@@ -6,22 +6,21 @@ class Solution:
         
         p_count = Counter(p)
         s_count = Counter()
-        # print(p_count,s_count)
         res = []
         
         for i in range(ns):
-            
+            # add the pointer to the window entering 
             s_count[s[i]] +=1
-            
+            # window complete
             if i >= np:
-                
+                # remove the last count if the count == 1
                 if s_count[s[i - np]] == 1:
                     del s_count[s[i - np]]
                 else:
-                    # second occpurance or aboe
+                    # reduce if not only 1
                     s_count[s[i - np]] -=1
             
-            # Comparing the hasmaps
+            # Comparing the hashmaps and add
             if p_count == s_count:
                 res.append(i - np + 1)
                 
